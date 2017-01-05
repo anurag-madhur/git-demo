0a943b2b5ef318d9fcc0b54aceb67877ef5fd6cd
Tushar <tushar@example.com>
2017-01-05 12:30:40 +0530

    include subtraction feature

--- add.c
+++ add.c
@@ -8,13 +8,20 @@ int add(int x,int y)
 
 }
 
+int sub(int x,int y)
+{
+	int z;
+	z=x-y;
+	return z;
+}
 void main()
 {
 	int a,b,c,result;
 	scanf("%d%d",&a,&b);
 	
 	c=add(a,b);
+	result=sub(a,b);
 	printf("Sum of the two no.s is %d",c);
-	
+	printf("subtraction of the no.s is %d"result);
 
 }
